9ce96573d0b9f6804d601416189f88027af83803
abdo2adel <abdo.awful@gmail.com>
2023-04-03 22:06:05 +0200

    table

---
+++ laravel-app/database/migrations/2023_04_03_195638_create_metro_timings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metro_timings', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('metro_timings');
+            $table->foreignId('metro_line_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metro_timings');
+    }
+};
