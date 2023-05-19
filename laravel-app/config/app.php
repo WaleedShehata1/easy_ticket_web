4385e91af8dad4e2f3d23b182c2cb95c35385150
omarkhaled24 <omarrkhaled260@gmail.com>
2023-05-19 18:21:15 +0300

    api

--- laravel-app/config/app.php
+++ laravel-app/config/app.php
@@ -195,6 +195,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Ichtrojan\Otp\OtpServiceProvider::class,
 
     ],
 
@@ -213,6 +214,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
     'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+    'Otp' => Ichtrojan\Otp\Otp::class,
     ])->toArray(),
-
+        
 ];
