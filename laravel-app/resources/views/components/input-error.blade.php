59765a501565cebfa58f542c5cefdb043bf86408
abdo2adel <abdo.awful@gmail.com>
2023-05-21 14:55:18 +0300

    new

--- laravel-app/resources/views/components/input-error.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['messages'])
-
-@if ($messages)
-    <ul style="margin:0px;">
-        @foreach ((array) $messages as $message)
-            <li>{{ $message}}</li>
-        @endforeach
-    </ul>
-@endif
