311c8a082df0febe6892450de7b704a8b0b2ac28
abdo2adel <abdo.awful@gmail.com>
2023-05-21 23:40:37 +0300

    mew

---
+++ laravel-app/app/Http/Controllers/api/EmailVerificationcontroller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Request\Api\EmailVerificationRequest;
+use otp;
+use App\Models\User;
+class EmailVerificationcontroller extends Controller
+{
+    private $otp;
+    public function _construct(){
+        $this ->otp = new otp;
+
+    }
+    public function email_verification(EmailVerificationRequest $request){
+        $otp22 = $this->otp->validate($request->email,$request->otp);
+        if(! $otp22->status){
+            return response()->json(['error'=>$otp22],404);
+        }
+        $user =User::where('email',$request->email)->first();
+        $user->update(['email_verified_at'=>now()]);
+        $Done['Done']=true;
+        return response()->json($Done,201);
+    }
+}
