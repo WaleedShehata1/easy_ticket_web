628c3780467d6ec2a30d32aa568cb3d95d877b9f
omarkhaled24 <omarrkhaled260@gmail.com>
2023-05-22 22:19:46 +0300

    api

--- laravel-app/app/Http/Controllers/api/EmailVerificationcontroller.php
+++ laravel-app/app/Http/Controllers/api/EmailVerificationcontroller.php
@@ -4,25 +4,25 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\Api\EmailVerificationRequest;
-use otp;
+use App\Http\Requests\Api\Emailverctionrequest;
+use Otp;
 use App\Models\User;
-use Illuminate\Contracts\Container\BindingResolutionException;
+// use Illuminate\Contracts\Container\BindingResolutionException;
 class EmailVerificationcontroller extends Controller 
 {
     private $otp;
-    public function _construct(){
-        $this ->otp = new otp;
+    public function __construct(){
+        $this->otp = new Otp;
 
     }
-    public function email_verification(EmailVerificationRequest $request){
+    public function email_verification(Emailverctionrequest $request){
         $otp22 = $this->otp->validate($request->email,$request->otp);
-        if(! $otp22->status){
-            return response()->json(['error'=>$otp22],404);
+        if(!$otp22->status){
+            return response()->json(['error' => $otp22],404);
         }
         $user =User::where('email',$request->email)->first();
-        $user->update(['email_verified_at'=>now()]);
-        $Done['Done']=true;
+        $user->update(['email_verified_at' => now()]);
+        $Done['Done'] = true;
         return response()->json($Done,201);
     }
 }
--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ApiController;
 use App\Http\Controllers\api\EmailVerificationcontroller;
+use App\Http\Controllers\api\ResetPasswordcontroller;
+use App\Http\Controllers\api\forgetpasswordcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,13 +27,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post("login",[ApiController::class,'login']);
 Route::post("register",[ApiController::class,'register']);
+Route::post("emailverification",[EmailVerificationcontroller::class,'email_verification']);
 
+Route::post("password/forget-password",[forgetpasswordcontroller::class,'forgetpassword']);
+Route::post("password/rest",[ResetPasswordcontroller::class,'passwordRest']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('/updatae/{id}', [ApiController::class, 'update']);
     Route::get('/show/{id}', [ApiController::class, 'show']);
     Route::post('/logout', [ApiController::class, 'logout']);
-    Route::post("emailverification",[EmailVerificationcontroller::class,'email_verification']);
+   
 });
 
 
